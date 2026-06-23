7370eac16f88e51c9f5fffedd61c5cdfc9a14acb
Jay Dhanoa <tophatraptor@users.noreply.github.com>
2026-06-23 11:34:27 -0700

    Add: shortest-word-distance - submission-1

---
+++ Data Structures & Algorithms/shortest-word-distance/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+        last_seen_word1 = -1
+        last_seen_word2 = -1
+        min_distance = len(wordsDict) + 1
+        # Move forward through the list
+        # Each time we see word1 or word2, we update index and recalculate
+        # min distance
+        for i, word in enumerate(wordsDict):
+            if word == word1:
+                last_seen_word1 = i
+            if word == word2:
+                last_seen_word2 = i
+            if last_seen_word1 >= 0 and last_seen_word2 >= 0:
+                diff = abs(last_seen_word2 - last_seen_word1)
+                if diff < min_distance:
+                    min_distance = diff
+        return min_distance
