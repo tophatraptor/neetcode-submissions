aa7cb6a571521d5664b2d45d7b344bfc7b02197a
Jay Dhanoa <tophatraptor@users.noreply.github.com>
2026-07-04 11:29:47 -0700

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        val_to_idx = {}
+        for i, x in enumerate(nums):
+            if x not in val_to_idx:
+                val_to_idx[x] = []
+            val_to_idx[x].append(i)
+        res = set()
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                target = - nums[i] - nums[j]
+                if target in val_to_idx:
+                    for k in val_to_idx[target]:
+                        if k != i and k != j:
+                            res.add(tuple(sorted([nums[i], nums[j], nums[k]])))
+        return [[i, j, k] for i, j, k in res]
