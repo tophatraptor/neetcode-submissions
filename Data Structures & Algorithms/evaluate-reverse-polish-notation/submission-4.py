08b9b46064a4d866d5f876e286f8017b4fc4a74e
Jay Dhanoa <tophatraptor@users.noreply.github.com>
2026-07-15 13:03:59 -0700

    Add: evaluate-reverse-polish-notation - submission-4

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        values = []
+        operators = ["+", "-", "*", "/"]
+        for x in tokens:
+            if x in operators:
+                v2 = values.pop()
+                v1 = values.pop()
+                if x == '+':
+                    values.append(v1 + v2)
+                elif x == '-':
+                    values.append(v1 - v2)
+                elif x == '*':
+                    values.append(v1 * v2)
+                elif x == '/':
+                    # We need to do floor division
+                    if (v1 < 0 or v2 < 0) and not (v1 < 0 and v2 < 0):
+                        sign = -1
+                    else:
+                        sign = 1
+                    values.append(sign * (abs(v1) // abs(v2)))
+            else:
+                x = int(x)
+                values.append(x)
+        return values[0]
+                    
